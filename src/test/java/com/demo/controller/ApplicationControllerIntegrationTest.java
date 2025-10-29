f14eadae9915f0e0051b151bdf67ff618d628d4d
vaidehi <bathrinath30@gmail.com>
2025-10-29 14:40:01 -0400

    Commenting Integration Tests

--- src/test/java/com/demo/controller/ApplicationControllerIntegrationTest.java
+++ src/test/java/com/demo/controller/ApplicationControllerIntegrationTest.java
@@ -13,8 +13,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
 public class ApplicationControllerIntegrationTest {
 
     @Autowired
