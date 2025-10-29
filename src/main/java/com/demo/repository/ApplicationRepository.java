d3378fdbe63fbd4c733cb84cd905037a4aba4774
vaidehi <bathrinath30@gmail.com>
2025-10-29 15:38:23 -0400

    Adding repository method

--- src/main/java/com/demo/repository/ApplicationRepository.java
+++ src/main/java/com/demo/repository/ApplicationRepository.java
@@ -4,6 +4,10 @@ import com.demo.repository.entity.Application;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ApplicationRepository extends JpaRepository<Application, Long> {
+
+    Optional<Application> findByName(String name);
 }
